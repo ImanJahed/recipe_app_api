2d74752bc29c9ccb07f9c4eca4e7a8aa2ff661a8
Iman Jahed <101338494+ImanJahed@users.noreply.github.com>
2024-02-11 12:04:50 +0330

    Added ingredient model and test

--- app/core/tests/test_ingredient_model.py
+++ app/core/tests/test_ingredient_model.py
@@ -1,20 +1,20 @@
-# """Test ingredient model"""
+"""Test ingredient model"""
 
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# from core import models
+from core import models
 
 
-# class IngredientTest(TestCase):
-#     """Test ingredient object"""
+class IngredientTest(TestCase):
+    """Test ingredient object"""
 
-#     def test_create_ingredient(self):
-#         """Test creating ingredient object in database"""
-#         user = get_user_model().objects.create_user(
-#             email='test@example.com',
-#             password='testpass1234'
-#             )
-#         ingredient = models.Ingredient.objects.create(name='Test Ingredient', user=user)
+    def test_create_ingredient(self):
+        """Test creating ingredient object in database"""
+        user = get_user_model().objects.create_user(
+            email='test@example.com',
+            password='testpass1234'
+            )
+        ingredient = models.Ingredient.objects.create(name='Test Ingredient', user=user)
 
-#         self.assertEqual(str(ingredient), ingredient.name)
+        self.assertEqual(str(ingredient), ingredient.name)
